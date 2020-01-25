41e33bc58affe7f9695366bccb4a41604636e493
guvenkaranfil <guvenkaranfil235@gmail.com>
2020-01-25 08:47:58 +0300

    create post page created

---
+++ pages/admin/post/Create.js
@@ -0,0 +1,124 @@
+import React, { Component } from "react";
+
+import Link from "next/link";
+import {
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  FormControl,
+  Form
+} from "react-bootstrap";
+import { Button } from "reactstrap";
+
+import { privateRoute } from "../../../components/PrivateRoute";
+import { createPost } from "../../../src/services/posts";
+
+class Create extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      content: "",
+      trailerContent: "",
+      tags: []
+    };
+  }
+
+  handleChange = e => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  onSubmit = () => {
+    console.log("this.stat ==> ", this.state);
+    const { title, content, trailerContent, tags } = this.state;
+    createPost(title, content, trailerContent, tags)
+      .then(response => {
+        console.log("response =?> ", response);
+        alert("post create");
+      })
+      .catch(err => {
+        console.log("err ==> ", err);
+        alert("err ==> ", err);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        {/* Contennt */}
+        <div className="box">
+          <label>Title:</label>
+          <textarea
+            onChange={this.handleChange}
+            name="title"
+            rows="1"
+            cols="33"
+          />
+        </div>
+
+        {/* Contennt */}
+        <div className="box">
+          <label>Content:</label>
+          <textarea
+            onChange={this.handleChange}
+            name="content"
+            rows="5"
+            cols="100"
+          />
+        </div>
+
+        {/* trailerContent */}
+        <div className="box">
+          <label>trailerContent:</label>
+          <textarea
+            onChange={this.handleChange}
+            name="trailerContent"
+            rows="2"
+            cols="50"
+          />
+        </div>
+
+        <button onClick={this.onSubmit} className="btn">
+          Share!
+        </button>
+
+        <style jsx>{`
+          label,
+          textarea {
+            font-size: 0.8rem;
+            letter-spacing: 1px;
+          }
+          textarea {
+            padding: 10px;
+            line-height: 1.5;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            box-shadow: 1px 1px 1px #999;
+          }
+
+          label {
+            display: block;
+            margin-bottom: 10px;
+          }
+          .box {
+            margin-top: 20px;
+          }
+          .btn {
+            width: 100px;
+            height: 30px;
+            border-radius: 15px;
+            margin: 50px;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
+
+export default privateRoute(Create);
---
+++ src/services/posts/index.js
@@ -0,0 +1,20 @@
+export const createPost = (title, content, trailerContent, tags) => {
+  return fetch(
+    "http://localhost:8000/api/post/create/5e2ac9a3b22bf774255127d5",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTJhYzlhM2IyMmJmNzc0MjU1MTI3ZDUiLCJpYXQiOjE1Nzk4NzIyOTJ9.HKTF0B6Law0mAtJyKGWslCEgnA7JY9PwCM69avmSp4o"
+      },
+      body: JSON.stringify({ title, content, trailerContent, tags })
+    }
+  )
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => {
+      return err;
+    });
+};
